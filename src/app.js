17994298e4433fb859d493b1cc05cd374946ca76
Lasang3012 <lasang3012@gmail.com>
2023-05-10 16:29:14 +0700

    Lesson 112: handling unhandled routes

--- src/app.js
+++ src/app.js
@@ -11,4 +11,11 @@ app.use(express.json());
 // routes
 app.use("/tours", tourRoutes);
 
+app.all("*", (req, res, next) => {
+  res.status(404).json({
+    status: "failed",
+    message: `Can't find ${req.originalUrl} on this server !`,
+  });
+});
+
 module.exports = app;
