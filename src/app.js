9425e908544f329742521825426b8c943e8f727b
Lasang3012 <lasang3012@gmail.com>
2023-05-10 17:20:16 +0700

    Lesson 114: Implementing a Global Error Handling Middleware

--- src/app.js
+++ src/app.js
@@ -12,9 +12,19 @@ app.use(express.json());
 app.use("/tours", tourRoutes);
 
 app.all("*", (req, res, next) => {
-  res.status(404).json({
-    status: "failed",
-    message: `Can't find ${req.originalUrl} on this server !`,
+  const err = new Error(`Can't find ${req.originalUrl} on this server !`);
+  err.status = "failed";
+  err.statusCode = 404;
+  next(err);
+});
+
+app.use((err, req, res, next) => {
+  err.statusCode = err.statusCode || 500;
+  err.status = err.status || "error";
+
+  res.status(err.statusCode).json({
+    status: err.status,
+    message: err.message,
   });
 });
 
--- src/routes/tour.routes.js
+++ src/routes/tour.routes.js
@@ -3,10 +3,6 @@ const tour = require("../controllers/tours/tour.controller");
 
 const router = express.Router();
 
-router.param("id", (req, res, next, value) => {
-  next();
-});
-
 router.route("/").get(tour.getTours).post(tour.createTour);
 router.route("/monthly-plan").get(tour.getMonthlyPlan);
 router.route("/import-data").post(tour.importTourData);
