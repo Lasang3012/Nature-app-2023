c19a19ae983f60048e8b92956cf16b07b0f4c943
Lasang3012 <lasang3012@gmail.com>
2023-05-20 17:26:37 +0700

    Lesson 129: Logging in Users

--- src/controllers/users/user.controller.js
+++ src/controllers/users/user.controller.js
@@ -1,17 +1,24 @@
 const userModule = require("../../modules/users/user.module");
 const catchAsync = require("../../utils/catch.error");
 const bcrypt = require("bcryptjs");
-const AppError = require("../../utils/app.error");
+const jwt = require("jsonwebtoken");
+require("dotenv").config();
 
 exports.createUser = catchAsync(async (req, res, next) => {
-  const newTourModule = new userModule();
+  const newUserModule = new userModule();
   const data = req.body;
   const salt = 10;
   data.password = await bcrypt.hash(data.password, salt);
   data.passwordConfirm = await bcrypt.hash(data.passwordConfirm, salt);
-  const newTour = await newTourModule.createUser(data);
+  const newUser = await newUserModule.createUser(data);
+  // jwt
+
+  const token = jwt.sign({ id: newUser.id }, process.env.JWT_SECRET, {
+    expiresIn: process.env.JWT_EXPIRES_IN,
+  });
   res.status(200).json({
     status: "success",
-    data: newTour,
+    token,
+    data: newUser,
   });
 });
