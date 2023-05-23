9c7e916a0e28cd7dc07262829447e93c177a7515
Lasang3012 <lasang3012@gmail.com>
2023-05-23 11:41:13 +0700

    Lesson 132: Protecting Tour Routes: Part 2

--- src/controllers/users/auth.controller.js
+++ src/controllers/users/auth.controller.js
@@ -1,5 +1,9 @@
 const AppError = require("../../utils/app.error");
 const catchAsync = require("../../utils/catch.error");
+const { promisify } = require("util");
+const jwt = require("jsonwebtoken");
+require("dotenv").config();
+const userModule = require("../../modules/users/user.module");
 
 exports.protect = catchAsync(async (req, res, next) => {
   let token;
@@ -12,5 +16,17 @@ exports.protect = catchAsync(async (req, res, next) => {
   if (!token) {
     return next(new AppError("login to get access", 500));
   }
+  const decoded = await promisify(jwt.verify)(token, process.env.JWT_SECRET);
+  const newUserModule = new userModule();
+  const user = await newUserModule.getUser({ id: decoded.id });
+  if (!user) {
+    return next(
+      new AppError(
+        "The user belonging to this token does no longer exist.",
+        401
+      )
+    );
+  }
+  req.user = user;
   next();
 });
